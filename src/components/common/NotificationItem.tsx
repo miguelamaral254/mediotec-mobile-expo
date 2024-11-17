4e2fb22b465ebd1b7e3dee827e870bc8839ab115
miguelamaral254 <miguel.amaral.sales@gmail.com>
2024-11-16 23:04:47 -0300

    Resolvido bug de não atualizar no banco o status da notificação

--- src/components/common/NotificationItem.tsx
+++ src/components/common/NotificationItem.tsx
@@ -4,6 +4,7 @@ import { Feather } from '@expo/vector-icons';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
 import { Notification } from '../../interfaces/notificationInterface';
 import { RootStackParamList } from '../../types/navigationTypes';
+import { updateNotificationReadStatus } from '../../services/notificationService';
 
 interface NotificationItemProps {
   notification: Notification;
@@ -18,24 +19,26 @@ const NotificationItem: React.FC<NotificationItemProps> = ({ notification, onRea
   const hoursDifference = timeDifference / (1000 * 60 * 60);
   const formattedDate = notificationTime.toLocaleDateString();
 
-  const handlePress = () => {
-    console.log("Abrindo detalhes para a notificação ID:", notification.id);  // Adicionando log para verificar o id ao abrir os detalhes
-    onRead(notification); 
-    navigation.navigate('NotificationDetails', { notification });  
+  const handlePress = async () => {
+    try {
+      await updateNotificationReadStatus({ id: notification.id, read: true });
+      onRead(notification);
+      navigation.navigate('NotificationDetails', { notification });
+    } catch (error) {
+      console.error("Erro ao atualizar o status de leitura:", error);
+    }
   };
 
   return (
     <TouchableOpacity
-      className={`flex-row items-center justify-between px-4 py-3 border-b ${
-        notification.read ? 'bg-gray-200 border-gray-300' : 'bg-white border-gray-400'
-      }`}
+      className={`flex-row items-center justify-between px-4 py-3 border-b ${notification.read ? 'bg-gray-200 border-gray-300' : 'bg-white border-gray-400'}`}
       onPress={handlePress}
     >
       <View className="flex-row items-center">
         <Feather
-          name={notification.read ? 'check-circle' : 'mail'}
+          name={notification.read ? "check-circle" : "mail"}
           size={24}
-          color={notification.read ? '#aaa' : '#000'}
+          color={notification.read ? "#aaa" : "#000"}
         />
         <View className="ml-2">
           <Text className={`font-bold ${notification.read ? 'text-gray-500' : 'text-black'}`}>
