e0e3e38614e1dd8ea1723f96e6e697de704c91e9
miguelamaral254 <miguel.amaral.sales@gmail.com>
2024-11-21 20:57:07 -0300

    ajustado

--- src/components/student/StudentFeed.tsx
+++ src/components/student/StudentFeed.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import { SchoolClass } from '../../interfaces/schoolClassInterface';
 import { translateEnum } from '../../utils/translateEnum';
-import { Link, NavigationProp } from '@react-navigation/native';
+import { NavigationProp } from '@react-navigation/native';
 import { StudentStackParamList } from '../../routes/StudentFeedRoutes';
 import { Linking } from 'react-native';
 import NoticeBoard from '../common/NoticeBoard';
@@ -12,16 +12,15 @@ import { User } from '../../interfaces/userInterface';
 
 interface StudentFeedProps {
   schoolClasses: SchoolClass[] | null;
-  userData: User | null; 
-  notifications: Notification[]; 
+  userData: User | null;
+  notifications: Notification[];
   navigation: NavigationProp<StudentStackParamList>;
 }
 
 const StudentFeed: React.FC<StudentFeedProps> = ({ schoolClasses, userData, navigation }) => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
-  const currentYear = new Date().getFullYear(); // Obtém o ano atual
+  const currentYear = new Date().getFullYear();
 
-  // Filtrar turmas do ano atual
   const currentYearClasses = schoolClasses
     ? schoolClasses.filter((schoolClass) => {
         const classYear = new Date(schoolClass.date).getFullYear();
@@ -34,7 +33,7 @@ const StudentFeed: React.FC<StudentFeedProps> = ({ schoolClasses, userData, navi
       if (userData?.cpf) {
         try {
           const response = await getNotificationsForUser(userData.cpf);
-          setNotifications(response || []); 
+          setNotifications(response || []);
         } catch (error) {
           console.error('Erro ao buscar notificações:', error);
         }
@@ -45,17 +44,19 @@ const StudentFeed: React.FC<StudentFeedProps> = ({ schoolClasses, userData, navi
   }, [userData?.cpf]);
 
   const handleOpenAva = () => {
-    const url = 'https://www.youtube.com/watch?v=4l15evegaKo&list=RDEMledDVnxwdhC8Hio8lzIxgQ&index=6';
+    const url =
+      'https://www.youtube.com/watch?v=4l15evegaKo&list=RDEMledDVnxwdhC8Hio8lzIxgQ&index=6';
     Linking.openURL(url).catch((err: Error) =>
       console.error('Erro ao abrir o link:', err)
     );
   };
+
   const handleOpenFinanceiro = () => {
     const url = 'https://www.youtube.com/watch?v=a4na2opArGY';
-    Linking.openURL(url).catch((err: Error)=>
-    console.error("Error ao abrir o link:", err)
-  );
-  }
+    Linking.openURL(url).catch((err: Error) =>
+      console.error('Erro ao abrir o link:', err)
+    );
+  };
 
   return (
     <ScrollView className="flex-1 bg-gray-100 p-4">
@@ -69,67 +70,93 @@ const StudentFeed: React.FC<StudentFeedProps> = ({ schoolClasses, userData, navi
         <NoticeBoard notifications={notifications} />
         {currentYearClasses.length > 0 ? (
           currentYearClasses.map((schoolClass) => (
-            <View key={schoolClass.id} className="mt-6 bg-gray-100 p-5 rounded-lg">
+            <View
+              key={schoolClass.id}
+              className="mt-6 bg-gray-100 p-5 rounded-lg"
+            >
               <Text className="text-xl font-semibold text-primary-color text-center">
-                {`Turma: ${schoolClass.code} - ${translateEnum(schoolClass.letter, 'letter')} (${translateEnum(schoolClass.shift, 'shift')})`}
+                {`Turma: ${schoolClass.code} - ${translateEnum(
+                  schoolClass.letter,
+                  'letter'
+                )} (${translateEnum(schoolClass.shift, 'shift')})`}
               </Text>
               <Text className="text-lg text-center">
-                {`${translateEnum(schoolClass.technicalCourse, 'technicalCourse')} - ${translateEnum(schoolClass.year, 'year')}`}
+                {`${translateEnum(
+                  schoolClass.technicalCourse,
+                  'technicalCourse'
+                )} - ${translateEnum(schoolClass.year, 'year')}`}
               </Text>
             </View>
           ))
         ) : (
-          <Text className="text-center text-gray-500">Nenhuma turma do ano atual encontrada</Text>
+          <Text className="text-center text-gray-500">
+            Nenhuma turma do ano atual encontrada
+          </Text>
         )}
       </View>
 
-      <View className="flex flex-row flex-wrap justify-between">
-        <TouchableOpacity
-          className="bg-white rounded-lg p-4 mb-4 w-1/2 shadow-md flex flex-col justify-between"
-          onPress={handleOpenAva}
-        >
-          <View>
+      {/* Grade de duas colunas com tamanhos fixos */}
+      <View className="flex flex-row flex-wrap justify-between mt-6">
+        {/* Card 1 */}
+        <View className="w-[48%] h-40 mb-4">
+          <TouchableOpacity
+            className="bg-white rounded-lg p-4 shadow-md h-full flex flex-col justify-between"
+            onPress={handleOpenAva}
+          >
             <Text className="text-lg font-bold text-primary-color mb-2">
               🖥️ Acesso ao AVA
             </Text>
-            <Text className="text-sm text-secondary-color mb-4">Fique em dia com suas atividades e tarefas.</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          className="bg-white rounded-lg p-4 mb-4 w-1/2 shadow-md flex flex-col justify-between"
-          onPress={handleOpenFinanceiro}
-        >
-          <View>
-            <Text className="text-lg font-bold text-primary-color mb-2">
-              🖥️ Acesso ao Financeiro
+            <Text className="text-sm text-secondary-color">
+              Fique em dia com suas atividades e tarefas.
             </Text>
-            <Text className="text-sm text-secondary-color mb-4">Fique em dia com suas atividades e tarefas.</Text>
-          </View>
-        </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          className="bg-white rounded-lg p-4 mb-4 w-1/2 shadow-md flex flex-col justify-between"
-          onPress={() => navigation.navigate('Contacts')}
-        >
-          <View>
+        {/* Card 2 */}
+        <View className="w-[48%] h-40 mb-4">
+          <TouchableOpacity
+            className="bg-white rounded-lg p-4 shadow-md h-full flex flex-col justify-between"
+            onPress={handleOpenFinanceiro}
+          >
+            <Text className="text-lg font-bold text-primary-color mb-2">
+              💰 Acesso ao Financeiro
+            </Text>
+            <Text className="text-sm text-secondary-color">
+              Consulte seus pagamentos e mensalidades.
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Card 3 */}
+        <View className="w-[48%] h-40 mb-4">
+          <TouchableOpacity
+            className="bg-white rounded-lg p-4 shadow-md h-full flex flex-col justify-between"
+            onPress={() => navigation.navigate('Contacts')}
+          >
             <Text className="text-lg font-bold text-primary-color mb-2">
               📞 Contato
             </Text>
-            <Text className="text-sm text-secondary-color mb-4">Entre em contato com seus professores ou a administração.</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity
-        className="bg-white rounded-lg p-4 mb-4 w-1/2 shadow-md flex flex-col justify-between"
-      >
-        <View>
-          <Text className="text-lg font-bold text-primary-color mb-2">
-            📅 Calendário Escolar <Text className="text-red-600 font-medium">WORK IN PROGRESS</Text>
-          </Text>
-          <Text className="text-sm text-secondary-color mb-4">Veja o calendário escolar e não perca nenhuma data importante.</Text>
+            <Text className="text-sm text-secondary-color">
+              Entre em contato com seus professores ou a administração.
+            </Text>
+          </TouchableOpacity>
         </View>
-      </TouchableOpacity>
+
+        {/* Card 4 */}
+        <View className="w-[48%] h-40 mb-4">
+          <TouchableOpacity
+            className="bg-white rounded-lg p-4 shadow-md h-full flex flex-col justify-between"
+          >
+            <Text className="text-lg font-bold text-primary-color mb-2">
+              📅 Calendário Escolar{' '}
+              <Text className="text-red-600 font-medium">WORK IN PROGRESS</Text>
+            </Text>
+            <Text className="text-sm text-secondary-color">
+              Veja o calendário escolar e não perca datas importantes.
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </ScrollView>
   );
 };
