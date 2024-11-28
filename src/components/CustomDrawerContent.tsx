490f3f8e401a5a6a553df231428a13b6f83aaf8b
miguelamaral254 <miguel.amaral.sales@gmail.com>
2024-11-27 22:07:52 -0300

    ajustado navbar

--- src/components/CustomDrawerContent.tsx
+++ src/components/CustomDrawerContent.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-import { DrawerContentScrollView, DrawerItemList, DrawerContentComponentProps } from '@react-navigation/drawer';
-import { User } from '../interfaces/userInterface';
+import React from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import {
+  DrawerContentScrollView,
+  DrawerContentComponentProps,
+} from "@react-navigation/drawer";
+import { User } from "../interfaces/userInterface";
 
 const styles = {
-  container: 'flex-1 bg-gray-900',
-  header: 'p-6 bg-blue-500 items-center',
-  image: 'w-20 h-20 rounded-full mb-4 border-2 border-white',
-  userName: 'text-white text-2xl font-bold text-center',
-  loadingText: 'text-white text-xl',
-  drawerList: 'flex-1 bg-gray-900',
-  footer: 'border-t border-gray-700 p-4 bg-gray-900',
-  footerButton: 'flex-row items-center mb-4',
-  footerIcon: 'text-white',
-  footerText: 'text-lg ml-3 text-white',
-  menuItem: 'flex flex-row items-center px-4 py-3',
-  menuIconFocused: 'text-white',
-  menuIconUnfocused: 'text-gray-400',
-  menuTextFocused: 'text-white font-bold text-lg',
-  menuTextUnfocused: 'text-gray-400 text-lg',
+  container: "flex-1 bg-blue-200",
+  header: "p-6 bg-blue-500 items-center",
+  image: "w-20 h-20 rounded-full mb-4 border-2 border-white",
+  userName: "text-white text-2xl font-bold text-center",
+  loadingText: "text-white text-xl",
+  drawerList: "flex-1 bg-gray-100",
+  footer: "border-t border-blue-200 p-4 bg-blue-500",
+  footerButton: "flex-row items-center mb-4",
+  footerIcon: "text-white",
+  footerText: "text-lg ml-3 text-white",
+  menuItem: "flex flex-row items-center px-4 py-3 rounded-sm",
+  menuIcon: "mr-3",
+  menuTextFocused: "text-white font-bold text-lg",
+  menuTextUnfocused: "text-blue-500 text-lg",
 };
-
 interface CustomDrawerContentProps extends DrawerContentComponentProps {
   onLogout: () => void;
   userData: User | null;
 }
 
-const CustomDrawerContent: React.FC<CustomDrawerContentProps> = ({ onLogout, userData, ...props }) => {
+const CustomDrawerContent: React.FC<CustomDrawerContentProps> = ({
+  onLogout,
+  userData,
+  ...props
+}) => {
   return (
     <DrawerContentScrollView
       {...props}
-      contentContainerStyle={{ flex: 1, backgroundColor: '#1E1E1E' }}
+      contentContainerStyle={{ flex: 1, backgroundColor: "#3B82F6" }}
     >
       <View className={styles.header}>
         <Image
-          source={require('../assets/avatar.png')}
+          source={require("../assets/avatar.png")}
           className={styles.image}
         />
         {userData ? (
@@ -48,36 +53,36 @@ const CustomDrawerContent: React.FC<CustomDrawerContentProps> = ({ onLogout, use
       {/* Menu Items */}
       <View className={styles.drawerList}>
         {props.state.routes.map((route, index) => {
-          if (route.name === 'settings') return null; // Oculta a rota "settings" da lista principal
-          
+          if (route.name === "settings") return null;
+
           const isFocused = props.state.index === index;
           let iconName: keyof typeof Feather.glyphMap;
           let label: string;
 
           switch (route.name) {
-            case 'home':
-              label = 'Início';
-              iconName = 'home';
+            case "home":
+              label = "Início";
+              iconName = "home";
               break;
-            case 'profile':
-              label = 'Meu Perfil';
-              iconName = 'user';
+            case "profile":
+              label = "Meu Perfil";
+              iconName = "user";
               break;
-            case 'grades':
-              label = 'Meu Boletim';
-              iconName = 'book';
+            case "grades":
+              label = "Meu Boletim";
+              iconName = "book";
               break;
-            case 'schedule':
-              label = 'Meu Horário';
-              iconName = 'calendar';
+            case "schedule":
+              label = "Meu Horário";
+              iconName = "calendar";
               break;
-            case 'relatedStudents':
-              label = 'Estudantes Relacionados';
-              iconName = 'users';
+            case "relatedStudents":
+              label = "Estudantes Relacionados";
+              iconName = "users";
               break;
             default:
               label = route.name;
-              iconName = 'file-text';
+              iconName = "file-text";
               break;
           }
 
@@ -85,15 +90,19 @@ const CustomDrawerContent: React.FC<CustomDrawerContentProps> = ({ onLogout, use
             <TouchableOpacity
               key={route.key}
               onPress={() => props.navigation.navigate(route.name)}
-              className={styles.menuItem}
+              className={`${styles.menuItem} ${isFocused ? "bg-blue-500" : ""}`}
             >
               <Feather
                 name={iconName}
                 size={24}
-                className={isFocused ? styles.menuIconFocused : styles.menuIconUnfocused}
+                style={{
+                  color: isFocused ? "#FFFFFF" : "#3B82F6",
+                }}
               />
               <Text
-                className={isFocused ? styles.menuTextFocused : styles.menuTextUnfocused}
+                className={
+                  isFocused ? styles.menuTextFocused : styles.menuTextUnfocused
+                }
               >
                 {label}
               </Text>
@@ -105,19 +114,17 @@ const CustomDrawerContent: React.FC<CustomDrawerContentProps> = ({ onLogout, use
       {/* Footer */}
       <View className={styles.footer}>
         <TouchableOpacity
-          onPress={() => props.navigation.navigate('settings')}
+          onPress={() => props.navigation.navigate("settings")}
           className={styles.footerButton}
         >
-          <Feather name="settings" size={24} className={styles.footerIcon} />
+          <Feather name="settings" size={24} className={styles.footerIcon} color= '#FFFFFF' />
           <Text className={styles.footerText}>Configurações</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={onLogout}
-          className={styles.footerButton}
-        >
-          <Feather name="log-out" size={24} className={styles.footerIcon} />
+        <TouchableOpacity onPress={onLogout} className={styles.footerButton}>
+          <Feather name="log-out" size={24} className={styles.footerIcon} color= '#FFFFFF' />
           <Text className={styles.footerText}>Logout</Text>
         </TouchableOpacity>
+        <Text className='text-white text-sm pt-5 ml-32'>Ver 1.1</Text>
       </View>
     </DrawerContentScrollView>
   );
