17fd9689b5797cc5a2b80f6ff7faccb61aec97ab
miguelamaral254 <miguel.amaral.sales@gmail.com>
2024-12-01 15:31:30 -0300

    nova grade de horario pra students

--- src/components/student/StudentSchedule.tsx
+++ src/components/student/StudentSchedule.tsx
@@ -1,24 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { getLessonsByStudentAndClass } from '../../services/lessonsService';
 import { Lesson, WeekDay, TimeSlot } from '../../interfaces/LessonInterface';
 import { mapWeekDayToPortuguese, mapTimeSlotToPortuguese } from '../../utils/mappingUtils';
-import LottieView from 'lottie-react-native';
 
 const daysOfWeek = Object.values(WeekDay);
 const timeSlots = Object.values(TimeSlot);
 
-const SCHEDULE_BOX_SIZE = 126;
-const HEADER_BOX_SIZE = 125;
-
-interface StudentScheduleProps {
-  cpf: string;
-  schoolClassId: number;
-}
-
-const StudentSchedule: React.FC<StudentScheduleProps> = ({ cpf, schoolClassId }) => {
+const StudentSchedule: React.FC<{ cpf: string; schoolClassId: number }> = ({ cpf, schoolClassId }) => {
   const [lessons, setLessons] = useState<Lesson[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [selectedDay, setSelectedDay] = useState<string | null>(daysOfWeek[0]);
+  const [selectedTime, setSelectedTime] = useState<string | null>(timeSlots[0]);
 
   useEffect(() => {
     const fetchLessons = async () => {
@@ -30,100 +23,103 @@ const StudentSchedule: React.FC<StudentScheduleProps> = ({ cpf, schoolClassId })
       } catch (error) {
         console.error('Error fetching lessons:', error);
       } finally {
-        setTimeout(() => {
-          setLoading(false);
-        }, 2000); // Adiciona um atraso de 2 segundos para exibir a animação
+        setLoading(false);
       }
     };
 
     fetchLessons();
   }, [cpf, schoolClassId]);
 
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F3F4F6' }}>
-        <LottieView
-          source={require('../../../assets/animations/calendar.json')} 
-          autoPlay
-          loop
-          style={{ width: 600, height:800 }}
-        />
-      </View>
+  const getSelectedLesson = () => {
+    if (!selectedDay || !selectedTime) return null;
+
+    return lessons.find(
+      (lesson) =>
+        lesson.weekDay === selectedDay && lesson.startTime === selectedTime
     );
-  }
+  };
 
-  const schedule = Array.from({ length: timeSlots.length }, () => Array(daysOfWeek.length).fill(null));
-
-  lessons.forEach((lesson) => {
-    const dayIndex = daysOfWeek.indexOf(lesson.weekDay);
-    const startIndex = timeSlots.indexOf(lesson.startTime);
-    if (dayIndex !== -1 && startIndex !== -1) {
-      schedule[startIndex][dayIndex] = lesson;
-    }
-  });
-
-  const timeIntervals = timeSlots.map((time, index) => {
-    const nextIndex = index + 1;
-    return nextIndex < timeSlots.length
-      ? `${mapTimeSlotToPortuguese(time)} às ${mapTimeSlotToPortuguese(timeSlots[nextIndex])}`
-      : null;
-  });
+  const selectedLesson = getSelectedLesson();
 
   return (
-    <ScrollView className="flex-1 p-5 bg-gray-100" horizontal showsHorizontalScrollIndicator={true}>
-      <View className="border border-gray-300 rounded-lg overflow-hidden">
-        <View className="flex flex-row bg-blue-500">
-          <Text
-            className="w-24 p-4 font-bold text-white text-center"
-            style={{ width: HEADER_BOX_SIZE }}
+    <View className="flex-1 bg-gray-100 p-4">
+      <View className="flex-row justify-between bg-white p-4 rounded-3xl mb-4">
+        {daysOfWeek.map((day) => (
+          <TouchableOpacity
+            key={day}
+            className={`rounded-lg px-4 py-2 ${
+              selectedDay === day ? "bg-blue-500" : "bg-white"
+            }`}
+            onPress={() => setSelectedDay(day)}
           >
-            Horário
-          </Text>
-          {daysOfWeek.map((day) => (
             <Text
-              key={day}
-              className={`w-${HEADER_BOX_SIZE} p-4 font-bold text-white text-center`}
-              style={{ width: HEADER_BOX_SIZE }}
+              className={`font-bold ${
+                selectedDay === day ? "text-white" : "text-gray-700"
+              }`}
             >
-              {mapWeekDayToPortuguese(day)}
+              {mapWeekDayToPortuguese(day)[0]}
             </Text>
-          ))}
-        </View>
-        {schedule.map((row, rowIndex) => (
-          <View
-            key={rowIndex}
-            className={`flex flex-row ${rowIndex % 2 === 0 ? 'bg-white' : 'bg-blue-100'}`}
-          >
-            <Text className="w-42 mt-8 p-5 text-blue-700 font-bold border-b border-gray-300 text-center">
-              {timeIntervals[rowIndex]}
-            </Text>
-            {row.map((lesson, colIndex) => (
-              <View
-                key={colIndex}
-                className="flex justify-center items-center border-b border-l border-gray-300"
-                style={{ width: SCHEDULE_BOX_SIZE, height: SCHEDULE_BOX_SIZE }}
-              >
-                {lesson ? (
-                  <View className="p-1 py-10 bg-blue-100 rounded-lg shadow-md">
-                    <Text className="text-center text-sm font-bold text-blue-700">
-                      {lesson.discipline?.name || 'Sem Disciplina'}
-                    </Text>
-                    <Text className="text-center text-xs text-blue-600">
-                      Professor: {lesson.professor.name || 'Não definido'}
-                    </Text>
-                    <Text className="text-center text-xs text-blue-600">
-                      Sala: {lesson.room || 'Não definido'}
-                    </Text>
-                  </View>
-                ) : (
-                  <Text className="text-center text-gray-400">- -</Text>
-                )}
-              </View>
-            ))}
-          </View>
+          </TouchableOpacity>
         ))}
       </View>
-    </ScrollView>
+
+      <View className="flex-row flex-1">
+        <View className="w-24 mr-4 items-center bg-white p-4 rounded-3xl">
+          <Text className="font-bold text-blue-700 mb-4">Horário</Text>
+          {timeSlots.map((time) => (
+            <TouchableOpacity
+              key={time}
+              className={`w-full py-2 rounded-lg mb-2 text-center ${
+                selectedTime === time ? "bg-blue-500" : "bg-white"
+              }`}
+              onPress={() => setSelectedTime(time)}
+            >
+              <Text
+                className={`font-bold text-center ${
+                  selectedTime === time ? "text-white" : "text-gray-700"
+                }`}
+              >
+                {mapTimeSlotToPortuguese(time)}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <View className="flex-1 shadow-md bg-white p-4 rounded-3xl">
+          <View className="items-center justify-center mb-4">
+            <Text className="text-lg  text-gray-700">INFORMAÇÕES</Text>
+          </View>
+          {selectedLesson ? (
+           <View>
+           <View className="mb-4">
+             <Text className="text-base font-bold text-blue-600">Professor:</Text>
+             <Text className="text-base text-gray-700">
+               {selectedLesson.professor?.name || "Não definido"}
+             </Text>
+           </View>
+         
+           <View className="mb-4">
+             <Text className="text-base font-bold text-blue-600">Matéria:</Text>
+             <Text className="text-base text-gray-700">
+               {selectedLesson.discipline?.name || "Sem Disciplina"}
+             </Text>
+           </View>
+         
+           <View className="mb-4">
+             <Text className="text-base font-bold text-blue-600">Sala:</Text>
+             <Text className="text-base text-gray-700">
+               {selectedLesson.room || "Não definido"}
+             </Text>
+           </View>
+         </View>
+          ) : (
+            <Text className="text-center text-gray-400">
+              Selecione um horário e um dia.
+            </Text>
+          )}
+        </View>
+      </View>
+    </View>
   );
 };
 
